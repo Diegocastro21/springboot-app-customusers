ec0cb0172fb3fed2b9b02431a5f820974c872ff0
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-07-06 09:52:54 -0500

    Subida

---
+++ src/main/java/com/castro/cu/SpringbootCustomUsersApplication.java
@@ -0,0 +1,13 @@
+package com.castro.cu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/castro/cu/SpringbootCustomUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castro.cu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
