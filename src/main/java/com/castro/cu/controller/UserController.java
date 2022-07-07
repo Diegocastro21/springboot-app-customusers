88d771aaa1870b79b720106ed8344e9c67eff9e0
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-07-07 04:43:54 -0500

    Implementando "Update User". Close #6

--- src/main/java/com/castro/cu/controller/UserController.java
+++ src/main/java/com/castro/cu/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -64,6 +65,51 @@ public class UserController {
         model.addAttribute("userList", userService.getAllUsers());
         model.addAttribute("roles", roleRepository.findAll());
         return "user-form/user-view";
+    }
+
+    @GetMapping("/editUser/{id}")
+    public String getEditUserForm(Model model, @PathVariable(name = "id")Long id)throws Exception{
+        User userToedit = userService.getUserById(id);
+        model.addAttribute("userForm", userToedit);
+        model.addAttribute("userList", userService.getAllUsers());
+        model.addAttribute("roles", roleRepository.findAll());
+        model.addAttribute("formTab","active");
+
+        model.addAttribute("editMode","true");
+
+        return "user-form/user-view";
+    }
+
+    @PostMapping("/editUser")
+    public String postEditUserForm(@Valid @ModelAttribute("userForm")User user, BindingResult result, ModelMap model){
+        if (result.hasErrors()) {
+            model.addAttribute("userForm", user);
+            model.addAttribute("formTab", "active");
+            model.addAttribute("editMode","true");
+        }else{
+            try {
+                userService.updateUser(user);
+                model.addAttribute("userForm", user);
+                model.addAttribute("listTab", "active");
+            } catch (Exception e) {
+                model.addAttribute("formErrorMessage", e.getMessage());
+                model.addAttribute("userForm", user);
+                model.addAttribute("formTab", "active");
+                model.addAttribute("userList", userService.getAllUsers());
+                model.addAttribute("roles", roleRepository.findAll());
+                model.addAttribute("editMode","true");
+            }
+        }
+
+
+        model.addAttribute("userList", userService.getAllUsers());
+        model.addAttribute("roles", roleRepository.findAll());
+        return "user-form/user-view";
+    }
+
+    @GetMapping("/editUser/cancel")
+    public String cancelEditUser(ModelMap model){
+        return "redirect:/userForm";
     }
 
 }
